63fe165da343fd05022eac919ca410e770389352
B-MNSN <manutsanan0@gmail.com>
2022-11-13 00:17:08 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,30 @@
-function Navbar() {
-    return(
-        <>
+import logo from "../image/logo_cusu.png";
+import { HiOutlineUserCircle } from "react-icons/hi";
 
-        </>
-    );
-};
+function Navbar() {
+  return (
+    <>
+      <div className="shadow">
+        <div className="d-flex justify-content-around p-2">
+          <a href="/"><img src={logo} alt="logo" href="" width={100} /></a>
+          <div className="">
+            <a href="/" className="mx-3 text-dark text-decoration-none">
+              HOME
+            </a>
+            <a href="/" className="mx-3 text-dark text-decoration-none">
+              WEDDING PLANNER
+            </a>
+            <a href="/" className="mx-3 text-dark text-decoration-none">
+              HOW TO
+            </a>
+          </div>
+          <div>
+            <HiOutlineUserCircle size={"30"} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Navbar;
