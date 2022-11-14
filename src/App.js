31c64c25e6fe82ecc62fc2d33539618333ef5f9b
B-MNSN <manutsanan0@gmail.com>
2022-11-14 23:22:58 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home';
+import WeddingPlanner from './pages/WeddingPlanner';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/wedding_planner' element={<WeddingPlanner/>}/>
+      </Routes>
+      
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import logo from "../image/logo_cusu.png";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { useState } from "react";
-import Modal from "./ModalOrganize";
+import Modal from "./ModalHowTo";
 
 function Navbar() {
   const [modalShow, setModalShow] = useState(false);
@@ -9,12 +9,12 @@ function Navbar() {
     <>
       <div className="shadow">
         <div className="d-flex justify-content-around p-2">
-          <a href="/"><img src={logo} alt="logo" href="" width={100} /></a>
+          <a href="/home"><img src={logo} alt="logo" href="" width={100} /></a>
           <div className="">
             <a href="/" className="mx-3 text-dark text-decoration-none">
               HOME
             </a>
-            <a href="/" className="mx-3 text-dark text-decoration-none">
+            <a href="/wedding_planner" className="mx-3 text-dark text-decoration-none">
               WEDDING PLANNER
             </a>
             <text className="mx-3 text-dark" onClick={() => setModalShow(true)}>
