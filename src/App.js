eee3c5bab1f02587a9b8686d1e0cd2798364bf88
Minnerproof <holyelifem@gmail.com>
2022-11-15 20:53:48 +0700

    day2-mk3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Addcard from './admin/Addcard';
 import Adddress from './admin/Adddress';
 import Addfood from './admin/Addfood';
+import Addgift from './admin/Addgift';
 import Addlocation from './admin/Addlocation';
+import Addorganize from './admin/Addorganize';
 import Addphoto from './admin/Addphoto';
 import Addtheme from './admin/Addtheme';
 import Adminbar from './admin/Adminbar';
@@ -16,7 +18,9 @@ function App() {
     {/* <Addfood/> */}
     {/* <Adddress/> */}
     {/* <Addphoto/> */}
-    <Addcard/>
+    {/* <Addcard/> */}
+    {/* <Addgift/> */}
+    <Addorganize/>
     {/* <Addlocation/> */}
     </>
   );
---
+++ src/admin/Addorganize.js
@@ -0,0 +1,37 @@
+import React from "react"
+import logo from "../image/logo_cusu.png";
+import { BiImageAdd } from "react-icons/bi";
+
+export default function Addorganize({ className }) {
+
+    return (
+        <div className={className}>
+            <div class=" background container-fluid text-center col-12">
+                <img src={logo} class="middle-logo col-4" alt="..." />
+                <div class="box-add container justify-content-center shadow-sm p-2 mb-5 rounded ">
+                    <h3 class=" py-3 fw-bolder d-flex justify-content-center ">องค์กร-บริษัท</h3>
+                    <div class="border-top border-4 border-dark "></div>
+
+                    <div class="col-12 d-flex justify-content-center">
+                        <button class="add-picture m-4" type="button"><h1><BiImageAdd></BiImageAdd></h1></button>
+
+                        <div className="col-8">
+                            <div class="m-4 d-flex justify-content-end">
+                                <input type="text" class="  form-control q-text container-fluid" placeholder='ชื่อ-องค์กร-บริษัท' id='title' required></input>
+                            </div>
+                            <div class="form-outline m-4 d-flex justify-content-end">
+                                <textarea class="form-control" id="textAreaExample" rows="4" placeholder='รายละเอียด-องค์กร-บริษัท'></textarea>
+                            </div>
+                            <div class="d-flex m-4 d-flex justify-content-end">
+                                <button type="button" class="btn btn-success btn-lg me-1">Confirm</button>
+                                <button type="button" class="btn btn-danger btn-lg me-1">Cancel</button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
