80688175ee2b66f06eebb0059160e05c34b70cec
B-MNSN <manutsanan0@gmail.com>
2022-11-17 20:34:11 +0700

    add data location, food, theme

--- src/admin/Add/Addtheme.js
+++ src/admin/Add/Addtheme.js
@@ -1,13 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import logo from "../../image/logo_cusu.png";
 import { BiImageAdd } from "react-icons/bi";
+import axios, { AxiosError } from "axios";
 
 
 export default function Addtheme({ className }) {
+    const [theme_img, setThemeImg] = useState("");
+    const [theme_name, setThemeName] = useState("");
+    const [theme_description, setThemeDescription] = useState("");
+    const [theme_price, setThemePrice] = useState("");
+
+    const handChange = (fn) => {
+        return (event) => {
+            fn(event.target.value);
+        };
+    };
+
+    const onSubmit = async (e) => {
+        try {
+          e.preventDefault();
+          const addTheme = await axios.post("http://localhost:4001/theme", {
+            theme_img,
+            theme_name,
+            theme_description,
+            theme_price,
+          });
+          console.log(addTheme);
+          localStorage.setItem("status", JSON.stringify(addTheme.data.status));
+
+        } catch (error) {
+          console.error(error);
+          if (error instanceof AxiosError) {
+            console.error(error.message);
+          }
+        }
+      };
 
     return (
         <div className={className}>
-            <div class=" background container-fluid text-center col-12">
+            <form class=" background container-fluid text-center col-12" onSubmit={onSubmit}>
                 <img src={logo} class="middle-logo col-4" alt="..." />
                 <div class="box-add container justify-content-center shadow-sm p-2 mb-5  rounded ">
                     <h3 class=" py-3 fw-bolder d-flex justify-content-center ">ธีม</h3>
@@ -18,18 +49,18 @@ export default function Addtheme({ className }) {
 
                         <div className="col-8">
                             <div class="m-4 d-flex justify-content-end">
-                                <input type="text" class="  form-control q-text container-fluid" placeholder='ชื่อ-ธีม' id='title' required></input>
+                                <input type="text" class="  form-control q-text container-fluid" placeholder='ชื่อ-ธีม' id='title' value={theme_name} onChange={handChange(setThemeName)}></input>
                             </div>
 
                             <div class="form-outline m-4 d-flex justify-content-end">
-                                <textarea class="form-control" id="textAreaExample" rows="4" placeholder='รายละเอียด-ธีม'></textarea>
+                                <textarea class="form-control" id="textAreaExample" rows="4" placeholder='รายละเอียด-ธีม' value={theme_description} onChange={handChange(setThemeDescription)}></textarea>
                             </div>
 
                             <div class="m-4 d-flex justify-content-end">
-                                <input type="text" class="  form-control q-text container-fluid " placeholder='ราคา-ธีม' id='title' required></input>
+                                <input type="text" class="  form-control q-text container-fluid " placeholder='ราคา-ธีม' id='title' value={theme_price} onChange={handChange(setThemePrice)}></input>
                             </div>
                             <div class="d-flex m-4 d-flex justify-content-end">
-                                <button type="button" class="btnConfirm border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Confirm</button>
+                                <button type="submit" class="btnConfirm border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Confirm</button>
                                 <button type="button" class="btnCancel border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Cancel</button>
                             </div>
 
@@ -37,7 +68,7 @@ export default function Addtheme({ className }) {
                     </div>
 
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
