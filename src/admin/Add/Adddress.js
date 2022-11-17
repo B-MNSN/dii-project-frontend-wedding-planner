d013f2e508aa8f37ce88002479dd55d44b616379
B-MNSN <manutsanan0@gmail.com>
2022-11-17 21:42:17 +0700

    post card, dress, organize, gift, photo

--- src/admin/Add/Adddress.js
+++ src/admin/Add/Adddress.js
@@ -1,12 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 import logo from "../../image/logo_cusu.png";
 import { BiImageAdd } from "react-icons/bi";
+import axios, { AxiosError } from "axios";
 
 export default function Adddress({ className }) {
+    const [dress_name, setDressName] = useState('');
+    const [dress_description, setDressDescription] = useState('');
+    const [dress_price, setDressPrice] = useState('');
+
+    const handChange = (fn) => {
+        return (event) => {
+            fn(event.target.value);
+        };
+    };
+
+    const onSubmit = async (e) => {
+        try {
+          e.preventDefault();
+          const adddress = await axios.post("http://localhost:4001/dress", {
+            dress_name,
+            dress_description,
+            dress_price
+          });
+          console.log(adddress);
+          localStorage.setItem("status", JSON.stringify(adddress.data.status));
+
+        } catch (error) {
+          console.error(error);
+          if (error instanceof AxiosError) {
+            console.error(error.message);
+          }
+        }
+      };
 
     return (
         <div className={className}>
-            <div class=" background container-fluid text-center col-12">
+            <form class=" background container-fluid text-center col-12" onSubmit={onSubmit}>
                 <img src={logo} class="middle-logo col-4" alt="..." />
                 <div class="box-add container justify-content-center shadow-sm p-2 mb-5 rounded ">
                     <h3 class=" py-3 fw-bolder d-flex justify-content-center ">ชุดแต่งงาน</h3>
@@ -17,17 +46,17 @@ export default function Adddress({ className }) {
 
                         <div className="col-8">
                             <div class="m-4 d-flex justify-content-end">
-                                <input type="text" class="  form-control q-text container-fluid" placeholder='ชื่อ-ชุดแต่งงาน' id='title' required></input>
+                                <input type="text" class="  form-control q-text container-fluid" placeholder='ชื่อ-ชุดแต่งงาน' id='title' value={dress_name} onChange={handChange(setDressName)}></input>
                             </div>
                             <div class="form-outline m-4 d-flex justify-content-end">
-                                <textarea class="form-control" id="textAreaExample" rows="4" placeholder='รายละเอียด-ชุดแต่งงาน'></textarea>
+                                <textarea class="form-control" id="textAreaExample" rows="4" placeholder='รายละเอียด-ชุดแต่งงาน' value={dress_description} onChange={handChange(setDressDescription)}></textarea>
                             </div>
 
                             <div class="m-4 d-flex justify-content-end">
-                                <input type="text" class="  form-control q-text container-fluid " placeholder='ราคา-ชุดแต่งงาน' id='title' required></input>
+                                <input type="text" class="  form-control q-text container-fluid " placeholder='ราคา-ชุดแต่งงาน' id='title' value={dress_price} onChange={handChange(setDressPrice)}></input>
                             </div>
                             <div class="d-flex m-4 d-flex justify-content-end">
-                            <button type="button" class="btnConfirm border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Confirm</button>
+                            <button type="submit" class="btnConfirm border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Confirm</button>
                                 <button type="button" class="btnCancel border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Cancel</button>
                             </div>
 
@@ -35,7 +64,7 @@ export default function Adddress({ className }) {
                     </div>
 
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
--- src/admin/Add/Addorganize.js
+++ src/admin/Add/Addorganize.js
@@ -1,12 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import logo from "../../image/logo_cusu.png";
 import { BiImageAdd } from "react-icons/bi";
+import axios, { AxiosError } from "axios";
 
 export default function Addorganize({ className }) {
+    const [organiz_name, setOrganizName] = useState('');
+    const [organiz_description, setOrganizDescription] = useState('');
+    
+    const handChange = (fn) => {
+        return (event) => {
+            fn(event.target.value);
+        };
+    };
+
+    const onSubmit = async (e) => {
+        try {
+          e.preventDefault();
+          const addOrganize = await axios.post("http://localhost:4001/organiz", {
+            organiz_name,
+            organiz_description,
+          });
+          console.log(addOrganize);
+          localStorage.setItem("status", JSON.stringify(addOrganize.data.status));
+
+        } catch (error) {
+          console.error(error);
+          if (error instanceof AxiosError) {
+            console.error(error.message);
+          }
+        }
+      };
 
     return (
         <div className={className}>
-            <div class=" background container-fluid text-center col-12">
+            <form class=" background container-fluid text-center col-12" onSubmit={onSubmit}>
                 <img src={logo} class="middle-logo col-4" alt="..." />
                 <div class="box-add container justify-content-center shadow-sm p-2 mb-5 rounded ">
                     <h3 class=" py-3 fw-bolder d-flex justify-content-center ">องค์กร-บริษัท</h3>
@@ -17,13 +44,13 @@ export default function Addorganize({ className }) {
 
                         <div className="col-8">
                             <div class="m-4 d-flex justify-content-end">
-                                <input type="text" class="  form-control q-text container-fluid" placeholder='ชื่อ-องค์กร-บริษัท' id='title' required></input>
+                                <input type="text" class="  form-control q-text container-fluid" placeholder='ชื่อ-องค์กร-บริษัท' id='title' value={organiz_name} onChange={handChange(setOrganizName)}></input>
                             </div>
                             <div class="form-outline m-4 d-flex justify-content-end">
-                                <textarea class="form-control" id="textAreaExample" rows="4" placeholder='รายละเอียด-องค์กร-บริษัท'></textarea>
+                                <textarea class="form-control" id="textAreaExample" rows="4" placeholder='รายละเอียด-องค์กร-บริษัท' value={organiz_description} onChange={handChange(setOrganizDescription)}></textarea>
                             </div>
                             <div class="d-flex m-4 d-flex justify-content-end">
-                            <button type="button" class="btnConfirm border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Confirm</button>
+                            <button type="submit" class="btnConfirm border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Confirm</button>
                                 <button type="button" class="btnCancel border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Cancel</button>
                             </div>
 
@@ -31,7 +58,7 @@ export default function Addorganize({ className }) {
                     </div>
 
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
