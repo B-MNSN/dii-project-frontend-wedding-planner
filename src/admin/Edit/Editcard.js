eb81a60a1b8cf53a78ede6915ccce92ff5357fa5
Minnerproof <holyelifem@gmail.com>
2022-11-19 16:44:03 +0700

    day6.mk2

--- src/admin/Edit/Editcard.js
+++ src/admin/Edit/Editcard.js
@@ -27,8 +27,8 @@ export default function Editcard({ className }) {
                                 <input type="text" class="  form-control q-text container-fluid " placeholder='ราคา-การ์ดแต่งงาน' id='title' required></input>
                             </div>
                             <div class="d-flex m-4 d-flex justify-content-end">
-                            <button type="button" class="btnCancel border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Cancel</button>
-                                <button type="submit" class="btnConfirm border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Confirm</button>
+                            <a href="/my_wedding_planner"><button class="btnCancel border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Cancel</button></a>
+                            <a href="/"><button class="btnConfirm border-0 rounded-2 text-light m-2 px-4 py-1 btn-lg">Confirm</button></a> 
                             </div>
 
                         </div>
